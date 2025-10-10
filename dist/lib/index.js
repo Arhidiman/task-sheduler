1a6d2202ac5570fe456b673f57124b79f702bef4
Dmitry Arkhipov <arhidiman@mail.ru>
2025-10-11 00:51:54 +0700

    chore: dist

--- dist/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.delayedExec = void 0;
-const delayedExec = (timeout, result) => {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(result), timeout);
-    });
-};
-exports.delayedExec = delayedExec;
